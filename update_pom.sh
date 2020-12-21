ff0ac0019b5c536f2a91e54b42c7ab4d8eec6b6d
kaushikarvind <56485562+kaushikarvind@users.noreply.github.com>
2020-12-21 10:58:03 +0000

    Update update_pom.sh

--- update_pom.sh
+++ update_pom.sh
@@ -1,4 +1,19 @@
 #! /bin/sh
 
+#cat pom.xml
+#cat sealights.json
+#echo $app_name
+#echo $branch_name
+#echo $build_name
+
 java -jar sl-build-scanner.jar -restorePom -workspacepath .
 java -jar sl-build-scanner.jar -pom -configfile sealights.json -workspacepath .
+
+sed -i "s/\(<appName>\)[^<]*\(<\/appName>\)/\1${app_name}\2/" pom.xml
+sed -i "s/\(<build>\)[^<]*\(<\/build>\)/\1${build_name}\2/" pom.xml
+sed -i "s/\(<branch>\)[^<]*\(<\/branch>\)/\1${branch_name}\2/" pom.xml
+
+#echo "***### Hello !!! Printing update POM for Diagnostics ###***"
+#cat pom.xml
+#cat sealights.json
+#echo "***### Diagnostics End ###***"
